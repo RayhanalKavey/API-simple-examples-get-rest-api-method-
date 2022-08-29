209415b3b2c3c7c5c255624f72d41001612f4c15
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-29 11:40:09 +0600

    show a card when clicked. Pass the card id into a onclinck button and display the card into the selected field.

--- js/mealdb.js
+++ js/mealdb.js
@@ -9,13 +9,13 @@ const getMeals = (meals) => {
   // console.log(meals);
   meals.forEach((meal) => {
     // console.log(meal);
-    const { strMeal, strMealThumb, strInstructions } = meal;
+    const { strMeal, strMealThumb, strInstructions, idMeal } = meal;
     // Create and append card
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
  
-            <div class="card h-100">
+            <div onclick="displayMealDetails(${idMeal})" class="card h-100">
               <img src="${strMealThumb}" class="card-img-top" alt="" />
               <div class="card-body">
                 <h5 class="card-title">${strMeal}</h5>
@@ -35,5 +35,35 @@ const searchFood = () => {
   // console.log(searchText);
   loadMeals(searchText);
   searchField.value = "";
+};
+const displayMealDetails = (searchID) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${searchID}`;
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => loadMealDetails(data.meals[0]));
+};
+
+const loadMealDetails = (mealDetails) => {
+  const { strMealThumb, strMeal, strInstructions } = mealDetails;
+  const mealDetailsField = document.getElementById("detail-container");
+  mealDetailsField.innerHTML = "";
+  const mealDiv = document.createElement("div");
+  mealDiv.classList.add("col");
+  mealDiv.innerHTML = `
+
+            <div class="card h-100">
+              <img src="${strMealThumb}" class="card-img-top" alt="" />
+              <div class="card-body">
+                <h5 class="card-title">${strMeal}</h5>
+                <p class="card-text">
+                 ${strInstructions.slice(0, 200)}
+                </p>
+              </div>
+            </div>
+
+    `;
+  mealDetailsField.appendChild(mealDiv);
+  // console.log(mealDetails);
 };
 loadMeals("a");
