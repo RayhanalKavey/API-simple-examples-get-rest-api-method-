bd86425024d8920a3e4bce84a73bed9009b15e75
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-02 12:37:38 +0600

    only phone hunter page.

--- js/kanye-quotes.js
+++
@@ -1,9 +0,0 @@
-function loadQuote() {
-  fetch("https://api.kanye.rest/")
-    .then((res) => res.json())
-    .then((data) => displayQuotes(data));
-}
-const displayQuotes = function (quote) {
-  const blockQuote = document.getElementById("quote");
-  blockQuote.innerText = quote.quote;
-};
